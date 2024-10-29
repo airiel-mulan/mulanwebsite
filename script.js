bf89b199bc4a1f9460ac7bf02fbccf5ff84a553a
airiel-mulan <airielrafieq78@gmail.com>
2024-10-29 22:15:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const menuContent = document.getElementById("menu-content");
+    menuContent.style.display = menuContent.style.display === "block" ? "none" : "block";
+}
